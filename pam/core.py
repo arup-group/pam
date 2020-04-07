855d4520dd3852f819acb71f7900ca83f88ecc1a
fred.shone <fred.shone@arup.com>
2020-04-07 23:00:37 +0100

    adds plan filling method

--- pam/core.py
+++ pam/core.py
@@ -67,7 +67,18 @@ class Person:
                     raise TypeError(f"Person {self.pid} incorrect plan sequence")
 
         if not isinstance(self.plan[-1], Activity):
-            raise TypeError(f"Person {self.pid} plan does not end with Activity")
+            raise TypeError(f"Person plan does not end with Activity")
+
+        if not self.plan[0].start_time == minutes_to_datetime(0):
+            raise TypeError("First activity does not start at zero")
+
+        for i in range(self.length - 1):
+            if not self.plan[i].end_time == self.plan[i+1].start_time:
+                raise TypeError("Miss-match in adjoining activity end and start times")
+
+        if not self.plan[-1].end_time == minutes_to_datetime(24*60-1):
+            raise TypeError("Last activity does not end at 23:59:59")
+
         return True
 
     @property
@@ -88,6 +99,13 @@ class Person:
     def home_based(self):
         return self.first_activity.lower() == 'home'
 
+    def position_of(self, act='home'):
+        last = None
+        for seq, act in enumerate(self.plan):
+            if act.act.lower() == 'home':
+                last = seq
+        return last
+
     def add(self, p):
         """
         Safely add a new component to the plan.
@@ -98,12 +116,14 @@ class Person:
             if self.plan and not isinstance(self.plan[-1], Leg):  # enforce act-leg-act seq
                 raise UserWarning(f"Cannot add Activity to plan sequence.")
             self.plan.append(p)
+
         elif isinstance(p, Leg):
             if not self.plan:
                 raise UserWarning(f"Cannot add Leg as first component to plan sequence.")
             if not isinstance(self.plan[-1], Activity):  # enforce act-leg-act seq
                 raise UserWarning(f"Cannot add Leg to plan sequence.")
             self.plan.append(p)
+
         else:
             raise UserWarning(f"Cannot add type: {type(p)} to plan.")
 
@@ -119,6 +139,10 @@ class Person:
     def clear_plan(self):
         self.plan = []
 
+    def print_plan(self):
+        for seq, component in enumerate(self.plan):
+            print(f"{seq}:\t{component}")
+
     def remove_activity(self, seq):
         """
         Remove an activity from plan at given seq.
@@ -130,9 +154,9 @@ class Person:
         assert isinstance(self.plan[seq], Activity)
 
         if (seq == 0 or seq == self.length - 1) and self.closed_plan:  # remove pair
-                self.plan.pop(0)
-                self.plan.pop(self.length - 1)
-                return self.length-2, 1
+            self.plan.pop(0)
+            self.plan.pop(self.length - 1)
+            return self.length-2, 1
 
         elif seq == 0:  # remove first act
             self.plan.pop(seq)
@@ -146,16 +170,141 @@ class Person:
             self.plan.pop(seq)
             return seq-2, seq+1
 
-    def fill_activity(self, p_idx, s_idx, default='home'):
+    def fill_plan(self, p_idx, s_idx, default='home'):
+        """
+        Fill a plan after Activity has been removed.
+        :param p_idx: location of previous Activity
+        :param s_idx: location of subsequent Activity
+        :param default:
+        :return: True
+        """
 
-        # if not self.closed_plan:  # can ignore looping activity
-        #     if self.home_based:  # extend last home based activity
-        #     previous_act
+        if p_idx is not None and s_idx is not None:  # regular middle of plan activity
 
-        raise NotImplementedError
+            if self.plan[p_idx] == self.plan[s_idx]:  # combine activities
+
+                if p_idx == s_idx:  # this is a single remaining activity -> stay at home
+
+                    if self.position_of(act='home') is None:
+                        raise ValueError(
+                            "Require home activity"
+                        )
+                    self.plan = self.stay_at_home()
+                    return True
+
+                if s_idx < p_idx:  # this is a wrapped activity --> close it
+                    # todo probably don't need to pass the idx - know that it must be first and last
+                    self.combine_wrapped_activities(p_idx, s_idx)
+                    return True
+
+                # this is a regular non wrapped mid plan activity -> combine acts
+                self.combine_matching_activities(p_idx, s_idx)
+                return True
+
+            # this plan cannot be closed - the proceeding and subs acts are not the same
+
+            if s_idx < p_idx:  # this is a wrapped activity --> close it
+                self.plan.pop(0)  # remove start leg
+                self.plan.pop(-1)  # remove end leg
+
+                pivot_idx = self.position_of(act='home')
+                if pivot_idx is None:
+                    raise NotImplementedError("Cannot fill plan without existing home activity")
+
+                self.expand(pivot_idx)
+                return True
+
+            # need to change first leg for new destination
+            self.join_activities(p_idx, s_idx)
+            return True
+
+        if p_idx is None:  # start of day non wrapping
+            self.plan.pop(0)
+            self.expand(s_idx-1)  # shifted because we popped index 0
+            return True
+
+        if s_idx is None:  # end of day non wrapping
+            self.plan.pop(-1)
+            self.expand(p_idx)
+            return True
+
+    def join_activities(self, p_idx, s_idx):
+        """Join two Activities with new Leg, expand last home activity"""
+        self.plan[p_idx + 1].end_area = self.plan[s_idx - 1].end_area
+        self.plan.pop(s_idx - 1)  # remove second leg
+
+        # todo add logic to change mode and time of leg
+
+        # press plans away from pivoting activity
+        pivot_idx = self.position_of(act='home')
+        if pivot_idx is None:
+            raise NotImplementedError("Cannot fill plan without existing home activity")
+
+        self.expand(pivot_idx)
+
+    def combine_matching_activities(self, p_idx, s_idx):
+        """Combine given Activities, remove surplus Legs"""
+        self.plan[p_idx].end_time = self.plan[s_idx].end_time  # extend proceeding act
+        self.plan.pop(s_idx)  # remove subsequent activity
+        self.plan.pop(s_idx - 1)  # remove subsequent leg
+        self.plan.pop(p_idx + 1)  # remove proceeding leg
+
+    def combine_wrapped_activities(self, p_idx, s_idx):
+        """Combine given Activities, remove surplus Legs"""
+        # extend proceeding act to end of day
+        self.plan[p_idx].end_time = minutes_to_datetime(24 * 60 - 1)
+        # extend subsequent act to start of day
+        self.plan[s_idx].start_time = minutes_to_datetime(0)
+        self.plan.pop(p_idx + 1)  # remove proceeding leg
+        self.plan.pop(s_idx - 1)  # remove subsequent leg
+        return True
+
+    def expand(self, pivot_idx):
+        # todo this isn't great - just pushes other activities to edges of day
+
+        new_time = minutes_to_datetime(0)
+        for seq in range(pivot_idx+1):  # push forward pivot and all proceeding components
+            new_time = self.plan[seq].shift_start_time(new_time)
+
+        new_time = minutes_to_datetime(24*60-1)
+        for seq in range(self.length-1, pivot_idx, -1):  # push back all subsequent components
+            new_time = self.plan[seq].shift_end_time(new_time)
+
+        self.plan[pivot_idx].end_time = new_time  # expand pivot
+
+    def stay_at_home(self):
+        return [
+            Activity(
+                seq=1,
+                act='home',
+                area=self.home,
+                start_time=minutes_to_datetime(0),
+                end_time=minutes_to_datetime(24 * 60 - 1),
+            )
+        ]
 
 
-class Activity:
+class PlanComponent:
+
+    @property
+    def duration(self):
+        return self.end_time - self.start_time
+
+    def shift_start_time(self, new_start_time):
+        duration = self.duration
+        self.start_time = new_start_time
+        self.end_time = new_start_time + duration
+        return self.end_time
+
+    def shift_end_time(self, new_end_time):
+        duration = self.duration
+        self.end_time = new_end_time
+        self.start_time = new_end_time - duration
+        return self.start_time
+
+
+class Activity(PlanComponent):
+
     def __init__(self, seq, act, area, start_time=None, end_time=None):
         # todo deal with times
         self.seq = seq
@@ -163,16 +312,17 @@ class Activity:
         self.area = area
         self.start_time = start_time
         self.end_time = end_time
-        # self.start_time_dt = dt.strptime(self.start_time, '%H:%M:%S')
-        # self.end_time_dt = dt.strptime(self.end_time, '%H:%M:%S')
-        # self.start_time_minutes = self.start_time_dt.hour * 60 + self.start_time_dt.minute
-        # self.end_time_minutes = self.end_time_dt.hour * 60 + self.end_time_dt.minute
-        # self.duration = self.end_time_minutes - self.start_time_minutes
-        # if self.duration < 0:
-        #     self.duration = (24 * 60) + self.duration
+
+    def __str__(self):
+        return f"Activity\t(seq:{self.seq}, act:{self.act}, area:{self.area}, " \
+               f"time:{self.start_time.time()} --> {self.end_time.time()}, " \
+               f"duration:{self.duration})"
+
+    def __eq__(self, other):
+        return (other.act, other.area) == (self.act, self.area)
 
 
-class Leg:
+class Leg(PlanComponent):
 
     act = 'travel'
 
@@ -193,14 +343,11 @@ class Leg:
         self.end_area = end_area
         self.start_time = start_time
         self.end_time = end_time
-        # self.start_time_dt = dt.strptime(start_time, '%H:%M:%S')
-        # self.end_time_dt = dt.strptime(end_time, '%H:%M:%S')
-        # self.start_time_minutes = self.start_time_dt.hour * 60 + self.start_time_dt.minute
-        # self.end_time_minutes = self.end_time_dt.hour * 60 + self.end_time_dt.minute
-        # self.duration = self.end_time_minutes - self.start_time_minutes
-        # self.dist = dist
-        # if self.duration < 0:
-        #     self.duration = (24 * 60) + self.duration
+
+    def __str__(self):
+        return f"Leg\t(seq:{self.seq}, mode:{self.mode}, area:{self.start_area} --> " \
+               f"{self.end_area}, time:{self.start_time.time()} --> {self.end_time.time()}, " \
+               f"duration:{self.duration})"
 
 
 def minutes_to_datetime(minutes: int):
@@ -213,3 +360,4 @@ def minutes_to_datetime(minutes: int):
     hours = minutes // 60
     minutes = minutes % 60
     return datetime(2020, 4, 2, hours, minutes)
+
--- tests/test_1_core.py
+++ tests/test_1_core.py
@@ -125,6 +125,18 @@ def test_person_not_closed_plan_different_areas():
     assert not person.closed_plan
 
 
+def test_activity_equal():
+    assert Activity(1, 'work', 1) == Activity(3, 'work', 1)
+
+
+def test_activity_not_equal_areas():
+    assert not Activity(1, 'work', 2) == Activity(3, 'work', 1)
+
+
+def test_activity_not_equal_acts():
+    assert not Activity(1, 'home', 2) == Activity(3, 'work', 2)
+
+
 @pytest.fixture
 def person_home_education_home():
 
@@ -236,6 +248,192 @@ def person_work_home_work_closed():
 
 
 @pytest.fixture
+def person_work_home_shop_home_work_closed():
+
+    person = Person(1)
+    person.add(
+        Activity(
+            seq=1,
+            act='work',
+            area='a',
+            start_time=minutes_to_datetime(0),
+            end_time=minutes_to_datetime(60)
+        )
+    )
+    person.add(
+        Leg(
+            seq=1,
+            mode='car',
+            start_area='a',
+            end_area='b',
+            start_time=minutes_to_datetime(60),
+            end_time=minutes_to_datetime(90)
+        )
+    )
+    person.add(
+        Activity(
+            seq=2,
+            act='home',
+            area='b',
+            start_time=minutes_to_datetime(90),
+            end_time=minutes_to_datetime(120)
+        )
+    )
+    person.add(
+        Leg(
+            seq=2,
+            mode='car',
+            start_area='b',
+            end_area='c',
+            start_time=minutes_to_datetime(120),
+            end_time=minutes_to_datetime(190)
+        )
+    )
+    person.add(
+        Activity(
+            seq=3,
+            act='shop',
+            area='c',
+            start_time=minutes_to_datetime(190),
+            end_time=minutes_to_datetime(220)
+        )
+    )
+    person.add(
+        Leg(
+            seq=3,
+            mode='car',
+            start_area='c',
+            end_area='b',
+            start_time=minutes_to_datetime(220),
+            end_time=minutes_to_datetime(280)
+        )
+    )
+    person.add(
+        Activity(
+            seq=4,
+            act='home',
+            area='b',
+            start_time=minutes_to_datetime(280),
+            end_time=minutes_to_datetime(320)
+        )
+    )
+    person.add(
+        Leg(
+            seq=4,
+            mode='car',
+            start_area='b',
+            end_area='a',
+            start_time=minutes_to_datetime(320),
+            end_time=minutes_to_datetime(380)
+        )
+    )
+    person.add(
+        Activity(
+            seq=5,
+            act='work',
+            area='a',
+            start_time=minutes_to_datetime(380),
+            end_time=minutes_to_datetime(24 * 60 - 1)
+        )
+    )
+
+    return person
+
+
+@pytest.fixture
+def person_work_home_shop_home_work_not_closed():
+
+    person = Person(1)
+    person.add(
+        Activity(
+            seq=1,
+            act='work',
+            area='a',
+            start_time=minutes_to_datetime(0),
+            end_time=minutes_to_datetime(60)
+        )
+    )
+    person.add(
+        Leg(
+            seq=1,
+            mode='car',
+            start_area='a',
+            end_area='b',
+            start_time=minutes_to_datetime(60),
+            end_time=minutes_to_datetime(90)
+        )
+    )
+    person.add(
+        Activity(
+            seq=2,
+            act='home',
+            area='b',
+            start_time=minutes_to_datetime(90),
+            end_time=minutes_to_datetime(120)
+        )
+    )
+    person.add(
+        Leg(
+            seq=2,
+            mode='car',
+            start_area='b',
+            end_area='c',
+            start_time=minutes_to_datetime(120),
+            end_time=minutes_to_datetime(190)
+        )
+    )
+    person.add(
+        Activity(
+            seq=3,
+            act='shop',
+            area='c',
+            start_time=minutes_to_datetime(190),
+            end_time=minutes_to_datetime(220)
+        )
+    )
+    person.add(
+        Leg(
+            seq=3,
+            mode='car',
+            start_area='c',
+            end_area='d',
+            start_time=minutes_to_datetime(220),
+            end_time=minutes_to_datetime(280)
+        )
+    )
+    person.add(
+        Activity(
+            seq=4,
+            act='home',
+            area='d',
+            start_time=minutes_to_datetime(280),
+            end_time=minutes_to_datetime(320)
+        )
+    )
+    person.add(
+        Leg(
+            seq=4,
+            mode='car',
+            start_area='d',
+            end_area='a',
+            start_time=minutes_to_datetime(320),
+            end_time=minutes_to_datetime(380)
+        )
+    )
+    person.add(
+        Activity(
+            seq=5,
+            act='work',
+            area='a',
+            start_time=minutes_to_datetime(380),
+            end_time=minutes_to_datetime(24 * 60 - 1)
+        )
+    )
+
+    return person
+
+
+@pytest.fixture
 def person_work_home_work_not_closed():
 
     person = Person(1)
@@ -289,6 +487,7 @@ def person_work_home_work_not_closed():
 
     return person
 
+# Tests for removing activities from plans
 
 def test_home_education_home_remove_activity_education(person_home_education_home):
 
@@ -302,6 +501,7 @@ def test_home_education_home_remove_activity_education(person_home_education_hom
 def test_work_home_work_remove_first_activity_closed(person_work_home_work_closed):
 
     person = person_work_home_work_closed
+    assert person.closed_plan
     p_idx, s_idx = person.remove_activity(0)
     assert p_idx == 1
     assert s_idx == 1
@@ -311,25 +511,124 @@ def test_work_home_work_remove_first_activity_closed(person_work_home_work_close
 def test_work_home_work_remove_last_activity_closed(person_work_home_work_closed):
 
     person = person_work_home_work_closed
+    assert person.closed_plan
     p_idx, s_idx = person.remove_activity(4)
     assert p_idx == 1
     assert s_idx == 1
     assert [p.act for p in person.activities] == ['home']
 
+def test_work_home_work_shop_remove_first_activity_closed(person_work_home_shop_home_work_closed):
 
-def test_work_home_work_remove_first_activity_not_closed(person_work_home_work_closed):
+    person = person_work_home_shop_home_work_closed
+    assert person.closed_plan
+    p_idx, s_idx = person.remove_activity(0)
+    assert p_idx == 5
+    assert s_idx == 1
+    assert [p.act for p in person.activities] == ['home', 'shop', 'home']
 
-    person = person_work_home_work_closed
+
+def test_work_home_work_shop_remove_last_activity_closed(person_work_home_shop_home_work_closed):
+
+    person = person_work_home_shop_home_work_closed
+    assert person.closed_plan
+    p_idx, s_idx = person.remove_activity(8)
+    assert p_idx == 5
+    assert s_idx == 1
+    assert [p.act for p in person.activities] == ['home', 'shop', 'home']
+
+
+def test_work_home_work_remove_first_activity_not_closed(person_work_home_work_not_closed):
+
+    person = person_work_home_work_not_closed
+    assert not person.closed_plan
     p_idx, s_idx = person.remove_activity(0)
     assert p_idx is None
     assert s_idx == 1
     assert [p.act for p in person.activities] == ['home', 'work']
 
 
-def test_work_home_work_remove_last_activity_not_closed(person_work_home_work_closed):
+def test_work_home_work_remove_last_activity_not_closed(person_work_home_work_not_closed):
 
-    person = person_work_home_work_closed
-    p_idx, s_idx = person.remove_activity(0)
+    person = person_work_home_work_not_closed
+    assert not person.closed_plan
+    p_idx, s_idx = person.remove_activity(4)
     assert p_idx == 2
     assert s_idx is None
     assert [p.act for p in person.activities] == ['work', 'home']
+
+# Tests for filling plans
+
+
+def test_home_education_home_fill_activity(person_home_education_home):
+
+    person = person_home_education_home
+    p_idx, s_idx = person.remove_activity(2)
+    assert person.fill_plan(p_idx, s_idx)
+    assert person.length == 1
+    assert [p.act for p in person.activities] == ['home']
+    assert person.plan[0].start_time == minutes_to_datetime(0)
+    assert person.plan[-1].end_time == minutes_to_datetime(24*60-1)
+    assert person.valid_plan
+
+
+def test_work_home_work_fill_activity_closed(person_work_home_work_closed):
+
+    person = person_work_home_work_closed
+    p_idx, s_idx = person.remove_activity(0)
+    assert person.fill_plan(p_idx, s_idx)
+    assert person.length == 1
+    assert [p.act for p in person.activities] == ['home']
+    assert person.plan[0].start_time == minutes_to_datetime(0)
+    assert person.plan[-1].end_time == minutes_to_datetime(24 * 60 - 1)
+    assert person.valid_plan
+
+
+def test_work_home_shop_work_fill_activity_closed(person_work_home_shop_home_work_closed):
+
+    person = person_work_home_shop_home_work_closed
+    p_idx, s_idx = person.remove_activity(0)
+    assert person.fill_plan(p_idx, s_idx)
+    assert person.length == 5
+    assert [p.act for p in person.activities] == ['home', 'shop', 'home']
+    assert person.plan[0].start_time == minutes_to_datetime(0)
+    assert person.plan[-1].end_time == minutes_to_datetime(24 * 60 - 1)
+    assert person.valid_plan
+
+
+def test_work_home_work_fill_first_activity_not_closed(person_work_home_shop_home_work_not_closed):
+
+    person = person_work_home_shop_home_work_not_closed
+    p_idx, s_idx = person.remove_activity(0)
+    assert person.fill_plan(p_idx, s_idx)
+    assert person.length == 5
+    assert [p.act for p in person.activities] == ['home', 'shop', 'home']
+    assert person.plan[0].start_time == minutes_to_datetime(0)
+    assert person.plan[-1].end_time == minutes_to_datetime(24 * 60 - 1)
+    assert person.valid_plan
+
+
+def test_work_home_work_fill_mid_activity_not_closed(person_work_home_shop_home_work_not_closed):
+
+    person = person_work_home_shop_home_work_not_closed
+    duration = person.plan[2].duration
+    p_idx, s_idx = person.remove_activity(6)
+    assert person.fill_plan(p_idx, s_idx)
+    assert person.length == 7
+    assert [p.act for p in person.activities] == ['work', 'home', 'shop', 'work']
+    assert person.plan[0].start_time == minutes_to_datetime(0)
+    assert person.plan[-1].end_time == minutes_to_datetime(24 * 60 - 1)
+    assert person.plan[2].duration > duration  # todo fix bad test
+    assert person.valid_plan
+
+
+def test_work_home_work_add_first_activity_not_closed(person_work_home_work_not_closed):
+
+    person = person_work_home_work_not_closed
+    p_idx, s_idx = person.remove_activity(0)
+    assert person.fill_plan(p_idx, s_idx)
+    assert person.length == 3
+    assert [p.act for p in person.activities] == ['home', 'work']
+    assert person.plan[0].start_time == minutes_to_datetime(0)
+    assert person.plan[-1].end_time == minutes_to_datetime(24 * 60 - 1)
+    assert person.valid_plan
+
