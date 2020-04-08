2743d7d82600363fc4ced3c74d6ce3189e6aab7e
fred.shone <fred.shone@arup.com>
2020-04-08 18:55:32 +0100

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="pam",
+    version="0.0.1",
+    author="Fred Shone",
+    author_email="",
+    description="Pandemic Activity Modeller/Modifier",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arup-group/pam",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
