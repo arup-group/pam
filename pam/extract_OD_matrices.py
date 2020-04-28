30b3185f4f625e9b3f5646ea421bf61f61a52b43
Iseul Song <Iseul.Song@arup.com>
2020-04-28 15:45:22 +0100

    converted od table into od matrix. spot checks with the raw data done. needs to be reviewed

--- pam/extract_OD_matrices.py
+++ pam/extract_OD_matrices.py
@@ -1,13 +1,6 @@
-import os
 import pandas as pd
 from pam.activity import Plan, Activity, Leg
 from pam.core import Population, Household, Person
-
-# 1.if activity is travel, I'd like to add origin and destination locations into a list respectively
-# 2.create a dataframe which has the first column as ozone and the second column dzone
-# 3.pivot table
-# 4.Write csv (df.to_csv('OD_matrices.csv'))
-
 
 def extract_od(population):
     ozone = []
@@ -17,8 +10,8 @@ def extract_od(population):
         for pid, person in household.people.items():
             for p in person.plan:
                 if p.act == 'travel':
-                    o = p.start_location
-                    d = p.end_location
+                    o = p.start_location.area
+                    d = p.end_location.area
                     ozone.append(o)
                     dzone.append(d)
 
@@ -28,15 +21,9 @@ def extract_od(population):
     }
 
     df = pd.DataFrame(data = dataDict).set_index('ozone')
+    matrix = df.pivot_table(values='dzone', index='ozone', columns='dzone', fill_value=0, aggfunc=len)
 
-    print(df)
-
-    df.pivot_table(values='dzone', index='ozone', columns='dzone', fill_value=0, aggfunc=len)
-        
-    ## https://stackoverflow.com/questions/60079115/create-origin-destination-matrix-from-a-data-frame-in-python
-    ## https://stackoverflow.com/questions/56520616/is-it-possible-from-dataframe-transform-to-matrix
-    ## https://stackoverflow.com/questions/43298192/valueerror-grouper-for-something-not-1-dimensional/52090527
-    
+    matrix.to_csv('OD_matrix.csv')
 
 
 extract_od(population)
