f7cdd454f3f840e647aac1988f751d0b11e03e0d
Michael Fitzmaurice <Michael.Fitzmaurice@arup.com>
2020-05-04 15:47:38 +0100

    Fix test for OD matrix export

--- tests/test_od_matrices.py
+++ tests/test_od_matrices.py
@@ -1,13 +1,13 @@
-import pytest
 import os
-import pandas as pd
-from pam.activity import Plan, Activity, Leg
-from pam.core import Population, Household, Person
-from pam.extract_OD_matrices import extract_od
 
-def test_extract_od():
+from pam.activity import Activity, Leg
+from pam.core import Population, Household, Person
+from pam.od_matrices import extract_od
+
+
+def test_writes_od_matrix_to_expected_file(tmpdir):
     population = Population()
-    for hid in range(1,11):
+    for hid in range(1, 11):
         household = Household(hid)
         population.add(household)
 
@@ -18,12 +18,11 @@ def test_extract_od():
             person.add(Activity(2,'work', 'Southwark'))
             person.add(Leg(2,'car', start_area='Southwark', end_area='Barnet'))
             person.add(Activity(3,'work', 'Barnet'))
-            household.add(person)        
+            household.add(person)
 
-    assert extract_od(population, path=os.path.join(r'C:\Users\Iseul.Song\PythonProjects', 'OD_matrix_test.csv'))
+    od_matrix_file = os.path.join(tmpdir, "od_matrix_test.csv")
 
+    extract_od(population, od_matrix_file)
 
-
-    # if __name__ == "__main__":
-    #     test_extract_od()
-    ### why the line 27 and 28 don't seem to work? 
+    od_matrix_csv_string = open(od_matrix_file).read()
+    assert od_matrix_csv_string == 'ozone,Barnet,Southwark\nBarnet,0,20\nSouthwark,20,0\n'
