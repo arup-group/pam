3dc873259baac826f5cbb70abba81a2188f8245b
Iseul Song <Iseul.Song@arup.com>
2020-05-26 20:38:43 +0100

    updated the code for single dimension segmentation (leg/person/time); updated the test code; deleted duplicated codes in other files.

--- pam/write.py
+++ pam/write.py
@@ -43,38 +43,73 @@ def write_travel_diary(population, path, attributes_path=None):
         pd.DataFrame(record).to_csv(attributes_path)
 
 
-def write_od_matrices(population, path, type_seg=None, mode_seg=None, time_seg=None):
+def write_od_matrices(
+        population, 
+        path, 
+        leg_filter=None, 
+        person_filter=None, 
+        time_minutes_filter=None):
+
     """
 	Write a core population object to tabular O-D weighted matrices.
-	Optionally segment matrices by type of journey (most likelly based on occupation),
-	mode and/or time (ie peaks).
+	Optionally segment matrices by leg attributes(mode/ purpose), person attributes or specific time periods.
+    A single filter can be applied each time.
 	:param population: core.Population
-	:param type_seg: segmentation option tbc
-	:param mode_seg: segmentation option tbc
-	:param time_seg: segmentation option tbc
+    :param path: directory to write OD matrix files
+    :param leg_filter: select between 'Mode', 'Purpose'
+    :param person_filter: select between given attribute categories (column names) from person attribute data
+    :param time_minutes_filter: a list of tuples to slice times, 
+                                e.g. [(start_of_slicer_1, end_of_slicer_1), (start_of_slicer_2, end_of_slicer_2), ... ]
 	:return: None
 	"""
-    ozone = []
-    dzone = []
 
+    legs = []      
+    
     for hid, household in population.households.items():
         for pid, person in household.people.items():
-            for p in person.plan:
-                if p.act == 'travel':
-                    o = p.start_location.area
-                    d = p.end_location.area
-                    ozone.append(o)
-                    dzone.append(d)
+            for leg in person.legs:
+                data = {'Household ID': hid,
+                            'Person ID': pid,
+                            'Origin':leg.start_location.area,
+                            'Destination': leg.end_location.area,
+                            'Purpose': leg.purpose,
+                            'Mode': leg.mode,
+                            'Sequence': leg.seq,
+                            'Start time': leg.start_time,
+                            'End time': leg.end_time}                
+                if person_filter:
+                    legs.append({**data, **person.attributes})
+                else:
+                    legs.append(data)         
+                                
+    data_legs = pd.DataFrame(data=legs)
+    
+    if leg_filter or person_filter:
+        if leg_filter:
+            data_legs_grouped=data_legs.groupby(leg_filter)
+        if person_filter:
+            data_legs_grouped=data_legs.groupby(person_filter)              
+        for filter, leg in data_legs_grouped:
+            df = pd.DataFrame(data=leg, columns = ['Origin','Destination']).set_index('Origin')
+            matrix = df.pivot_table(values='Destination', index='Origin', columns='Destination', fill_value=0, aggfunc=len)
+            matrix.to_csv(os.path.join(path, filter+'_od.csv'))
 
-    data_dict = {
-        'ozone': ozone,
-        'dzone': dzone
-    }
-
-    df = pd.DataFrame(data=data_dict).set_index('ozone')
-    matrix = df.pivot_table(values='dzone', index='ozone', columns='dzone', fill_value=0, aggfunc=len)
-
-    matrix.to_csv(path)
+    if time_minutes_filter:
+        periods = []
+        for time in time_minutes_filter:
+            periods.append(time)               
+        for start_time, end_time in periods:
+            file_name = str(start_time) +'_to_'+ str(end_time)
+            start_time = mtdt(start_time)
+            end_time = mtdt(end_time)
+            data_time = data_legs[(data_legs['Start time']>= start_time)&(data_legs['Start time']< end_time)]
+            df = pd.DataFrame(data=data_time, columns = ['Origin','Destination']).set_index('Origin')        
+            matrix = df.pivot_table(values='Destination', index='Origin', columns='Destination', fill_value=0, aggfunc=len)
+            matrix.to_csv(os.path.join(path, 'time_'+file_name+'_od.csv'))
+        
+    df_total = pd.DataFrame(data=legs, columns = ['Origin','Destination']).set_index('Origin')              
+    matrix = df_total.pivot_table(values='Destination', index='Origin', columns='Destination', fill_value=0, aggfunc=len)
+    matrix.to_csv(os.path.join(path, 'total_od.csv'))
 
 
 def write_matsim(
