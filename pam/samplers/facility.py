adf899866d7e941471cb8087fb416933a361c457
fred.shone <fred.shone@arup.com>
2020-06-05 21:53:31 +0100

    ack

--- pam/samplers/facility.py
+++ pam/samplers/facility.py
@@ -1,10 +1,80 @@
 from typing import Union
-from geopandas import GeoSeries, GeoDataFrame
+import geopandas as gp
 from shapely.geometry import Point
 import random
 
 
-def random_point(idx: Union[int, str], geoms: Union[GeoSeries, GeoDataFrame], patience=100, fail=True):
+def inf_yielder(candidates):
+    """
+    Endlessly yield shuffled candidate items.
+    """
+    while True:
+        random.shuffle(candidates)
+        for c in candidates:
+            yield c
+
+
+def build_facilities_sampler(facilities, census_areas, activities):
+    """
+    Build facility location sampler from osmfs input.
+    """
+    activity_areas = gp.sjoin(facilities, census_areas, how='inner', op='intersects')
+    d = {}
+    for zone in set(activity_areas.index_right):
+        zone_facilities = activity_areas.loc[activity_areas.index_right == zone]
+        d[zone] = {}
+        for act in activities:
+            points = list(zone_facilities.loc[zone_facilities.activity == act].geometry)
+            if points:
+                d[zone][act] = inf_yielder(points)
+            else:
+                d[zone][act] = None
+    return d
+
+
+class FaciltySampler:
+
+    def __init__(self, facilities, census_areas, activities, build_xml=True, fail=True):
+        self.activities = activities
+        self.sampler = self.build_facilities_sampler(facilities, census_areas, activities)
+
+
+    def sample_facility(sampler, location, activity, random_default=True):
+        """
+        Sample a facilkty location 
+        """
+        if idx not in self.sampler:
+            print(f'could not find areas is {idx}')
+            return None
+        
+        sampler = osm_sampler_dict[idx][act]
+        if not sampler:
+            if random_sampler:
+                print(f'random_sample for {idx} {act}')
+                return random_sampler(idx, areas)
+        else:
+            return next(sampler)
+
+    @staticmethod
+    def build_facilities_sampler(facilities, census_areas, activities):
+        """
+        Build facility location sampler from osmfs input.
+        """
+        activity_areas = gp.sjoin(facilities, census_areas, how='inner', op='intersects')
+        d = {}
+        for zone in set(activity_areas.index_right):
+            zone_facilities = activity_areas.loc[activity_areas.index_right == zone]
+            d[zone] = {}
+            for act in activities:
+                points = list(zone_facilities.loc[zone_facilities.activity == act].geometry)
+                if points:
+                    d[zone][act] = inf_yielder(points)
+                else:
+                    d[zone][act] = None
+        return d
+
+
+def random_point(idx: Union[int, str], geoms: Union[gp.GeoSeries, gp.GeoDataFrame], patience=100, fail=True):
     """
     Returns randomly placed point within given geometry, using geoms. Note that it uses
     random sampling within the shape's bounding box then checks if point is within given geometry.
@@ -22,11 +92,11 @@ def random_point(idx: Union[int, str], geoms: Union[GeoSeries, GeoDataFrame], pa
             raise IndexError(f'Cannot find idx: {idx} in geoms (type: {type(geoms)})')
         return None
 
-    if isinstance(geoms, GeoSeries):
+    if isinstance(geoms, gp.GeoSeries):
         point =  sample_point_from_polygon(geoms[idx], patience=patience)
         
 
-    elif isinstance(geoms, GeoDataFrame):
+    elif isinstance(geoms, gp.GeoDataFrame):
         point =  sample_point_from_polygon(geoms.geometry[idx], patience=patience)
 
     else:
@@ -49,3 +119,5 @@ def sample_point_from_polygon(poly, patience=100):
             return random_point
 
     return None
+
+
