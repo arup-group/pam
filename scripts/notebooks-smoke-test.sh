0cc649755f8795c8e3bf5342c2bbd0dffc44ae20
JosePazNoguera <jose.paznoguera@gmail.com>
2021-09-15 11:59:44 +0100

    redesign_notebooks_dir

--- scripts/notebooks-smoke-test.sh
+++ scripts/notebooks-smoke-test.sh
@@ -4,7 +4,7 @@ set -e
 
 pushd "${0%/*}"/..
 
-python examples/notebook_smoke_tests.py -d notebooks \
+python scripts/notebook_smoke_tests.py -d examples \
 -k pam \
 -k python37364bit373pyenve4f55e1c90f74740a9da8a10ea80341d \
 -k python3
