af61380eec49b1d9f2eef35b3610a0fff6fda896
YannisZa <yzachos@gmail.com>
2022-01-04 17:30:58 +0200

    modified sampling scripts to be reproducible (seed parameter passed)

--- pam/samplers/basic.py
+++ pam/samplers/basic.py
@@ -1,14 +1,19 @@
 import random
 
 
-def freq_sample(freq: float, sample: float):
+def freq_sample(freq: float, sample: float, seed:int=None):
     """
     Down or up sample a frequency based on a sample size. Sub unit frequencies are
     rounded probabalistically.
     :param freq: pre sampled frequency (integer)
     :param sample: sample size (float)
+    :params int seed: seed number for reproducible results (None default - does not fix seed)
+
     :return: new frequency (integer)
     """
+    # Fix random seed
+    random.seed(seed)
+
     new_freq = freq * sample
     remainder = new_freq - int(new_freq)
     remainder = int(random.random() < remainder)
