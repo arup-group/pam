ac925390f6d2c1b13b56acf2e127cd9aa7cfbcc9
Divya Sharma <divya.sharma@LDNML0CEMD6P.local>
2022-05-05 14:46:16 +0100

    tour script, tests, and example notebook

--- tests/test_9_samplers_tour.py
+++ tests/test_9_samplers_tour.py
@@ -4,15 +4,50 @@ import pandas as pd
 import geopandas as gp
 from shapely.geometry import Point, Polygon
 
+from pam.core import Person
 from pam.samplers import tour
+from pam.samplers.facility import FacilitySampler
+from pam.variables import END_OF_DAY
+
+# Test input data
+facility_df = pd.DataFrame({'id':[1,2,3,4], 'activity': ['home','work','home','depot']})
+points = [Point((1,1)), Point((1,1)), Point((3,3)), Point((3,3))]
+facility_gdf = gp.GeoDataFrame(facility_df, geometry=points)
+
+
+zones_df = pd.DataFrame({'a':[1,2,3], 'b': [4,5,6]})
+polys = [
+        Polygon(((0,0), (0,2), (2,2), (2,0))),
+        Polygon(((2,2), (2,4), (4,4), (4,2))),
+        Polygon(((4,4), (4,6), (6,6), (6,4)))
+    ]
+
+zones_gdf = gp.GeoDataFrame(zones_df, geometry=polys)
+
+facility_sampler = FacilitySampler(
+    facilities=facility_gdf,
+    zones=zones_gdf,
+    build_xml=True,
+    fail=False,
+    random_default=True
+)
+
+
+def test_model_distance_value():
+    assert tour.ActivityDuration().model_distance(Point((0,0)), Point((3,4)), scale=1) == 5
+
+def test_model_journey_time_value():
+    assert tour.ActivityDuration().model_journey_time(1000, 10) == 100
+    assert tour.ActivityDuration().model_journey_time(50000, 40000/3600) == 4500
 
 
 def test_distribution_type():
     bins = range(0,24)
-    pivots = {7: 2.2, 8: 1.5, 9: 1.7, 10: 1.9}
+    pivots = {1: 0, 2: 1, 8: 2, 15: 3}
     total = 100
 
     dist = tour.CreateDistribution().build_distribution(bins=bins, pivots=pivots, total=total)
+    print(dist)
     
     assert isinstance(dist, dict)
 
@@ -22,18 +57,6 @@ def test_frequency_sampler_type():
 
 
 def test_facility_density_missing_activity():
-    facility_df = pd.DataFrame({'id':[1,2,3,4], 'activity': ['home','work','home','delivery']})
-    points = [Point((1,1)), Point((1,1)), Point((3,3)), Point((3,3))]
-    facility_gdf = gp.GeoDataFrame(facility_df, geometry=points)
-
-    zones_df = pd.DataFrame({'a':[1,2,3], 'b': [4,5,6]})
-    polys = [
-        Polygon(((0,0), (0,2), (2,2), (2,0))),
-        Polygon(((2,2), (2,4), (4,4), (4,2))),
-        Polygon(((4,4), (4,6), (6,6), (6,4)))
-    ]
-    zones_gdf = gp.GeoDataFrame(zones_df, geometry=polys)
-
     o_density, d_density = tour.InputDemand().facility_density(facilities=facility_gdf,
                                                                       zones=zones_gdf,
                                                                       zone_id='a',
@@ -41,5 +64,30 @@ def test_facility_density_missing_activity():
                                                                       d_activity='delivery')
 
 
-    assert len(o_density) == 0
-    assert len(d_density) > 0
+    assert len(o_density) > 0
+    assert len(d_density) == 0
+
+
+def test_od_columns():
+    df_origin, df_destination, df_od = tour.InputDemand().od_distance(zones=zones_gdf)
+
+    assert list(df_od.drop(['distance'], axis=1).columns) == (list(df_origin.columns) + list(df_destination.columns))
+
+
+def test_activity_endtm_endofday():
+    agent_id = 'LGV_1'
+    agent = Person(
+            agent_id,
+            attributes={
+                'subpopulation': 'lgv',
+                'CarType': 'lgv',
+                'CarCO2': 'lgv'
+            }
+        )
+    o_zone = random.choice(zones_gdf.a)
+    o_loc = facility_sampler.sample(o_zone, 'depot')
+
+    time_params = {'hour':tour.FrequencySampler(range(24)).sample(), 'minute':tour.FrequencySampler(range(60)).sample()}
+    end_tm = tour.TourPlan().tour_activity(agent=agent, k=1, zone=o_zone, loc=o_loc, activity_type='depot', time_params=time_params)
+    
+    assert end_tm != END_OF_DAY
