471664c1d6d58c4d6a26d5f6771e84d625e405e2
Theodore.Chatziioannou <Theodore.Chatziioannou@arup.com>
2022-06-30 17:09:20 +0100

    respond to comments

--- tests/test_16_cli.py
+++ tests/test_16_cli.py
@@ -31,17 +31,24 @@ def test_cli_cropping(path_test_plan, path_boundary, tmp_path):
     assert os.path.exists(os.path.join(path_output_dir, 'plans.xml'))
 
 
-def test_cli_sample(path_test_plan, tmp_path):
+@pytest.mark.parametrize('sample_percentage', ['1','2'])
+def test_cli_sample(path_test_plan, tmp_path, sample_percentage):
     """ Double the population of 5 agents """
     path_output_dir = str(tmp_path)
     runner = CliRunner()
-    result = runner.invoke(sample, [path_test_plan, path_output_dir, '-s' '2'])
+    result = runner.invoke(sample, [path_test_plan, path_output_dir, '-s', sample_percentage])
     assert result.exit_code == 0
     assert os.path.exists(os.path.join(path_output_dir, 'plans.xml'))
+
+    population_input = read.read_matsim(
+        path_test_plan,
+        household_key="hid",
+        version=12
+    )
 
     population = read.read_matsim(
         os.path.join(path_output_dir, 'plans.xml'),
         household_key="hid",
         version=12
     )
-    assert len(population) == 10
+    assert len(population) == (len(population_input) * float(sample_percentage))
