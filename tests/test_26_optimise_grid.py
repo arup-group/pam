88d3faab06088d1c5a749fa69e65c3aee9d56dc0
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2023-07-04 16:12:58 +0100

    Update test names; split out tests

--- tests/test_26_optimise_grid.py
+++ tests/test_26_optimise_grid.py
@@ -11,16 +11,23 @@ def recorder():
 
 
 class TestRecorder:
-    def test_recorder_init(self, recorder):
+    def test_init(self, recorder):
         assert isinstance(recorder.best_plan, Plan)
         assert recorder.best_score == 0
 
-    def test_recorder_do_update(self, recorder):
+    def test_higher_scoring_plan_updates_best_plan(self, recorder):
         recorder.update(1, Plan(home_area="bar"))
         assert recorder.best_score == 1
         assert recorder.best_plan.home_location.area == "bar"
 
-    def test_recorder_do_not_update(self, recorder):
+    def test_lower_scoring_plan_does_not_update_best_plan(self, recorder):
         recorder.update(-1, Plan(home_area="bar"))
         assert recorder.best_score == 0
         assert recorder.best_plan.home_location.area == "foo"
+
+    def test_higher_scoring_plan_deepcopies_best_plan(self, recorder):
+        "id() of Plan object and objects within Plan (e.g., `home_location`) change on deepcopy"
+        best_plan = Plan(home_area="bar")
+        recorder.update(1, best_plan)
+        assert id(recorder.best_plan) != id(best_plan)
+        assert id(recorder.best_plan.home_location) != id(best_plan.home_location)
--- tests/test_27_optimise_random.py
+++ tests/test_27_optimise_random.py
@@ -10,27 +10,40 @@ def stopper():
 
 
 class TestStopper:
-    def test_stopper_init(self, stopper):
+    def test_init(self, stopper):
         assert stopper.record == []
         assert stopper.horizon == 3
         assert stopper.sensitivity == 0.03
 
-    def test_stopper_ok_1(self, stopper):
+    def test_adds_to_record(self, stopper):
         assert stopper.ok(1) is True
         assert stopper.record == [1]
 
-    def test_stopper_ok_2(self, stopper):
-        assert stopper.ok(1) is True
-        assert stopper.ok(1.02) is True
-        assert stopper.record == [1, 1.02]
+    def test_adds_to_already_populated_record(self, stopper):
+        stopper.ok(1)
+        stopper.ok(1.02)
+        stopper.ok(2)
+        assert stopper.record == [1, 1.02, 2]
 
-    def test_stopper_ok_3_no_stop(self, stopper):
+    def test_pops_oldest_value_when_record_exceeds_horizon_length(self, stopper):
         for i in [1, 2, 3, 4]:
-            assert stopper.ok(i) is True
+            stopper.ok(i)
         assert stopper.record == [2, 3, 4]
 
-    def test_stopper_ok_3_stop(self, stopper):
+    def test_ok_is_true_when_sensitivity_value_not_breached(self, stopper):
+        for i in [1, 2, 3, 4]:
+            assert stopper.ok(i) is True
+
+    def test_ok_is_false_when_sensitivity_value_breached_and_record_exceeds_horizon_length(
+        self, stopper
+    ):
         for i in [1, 2, 2.01]:
             assert stopper.ok(i) is True
         assert stopper.ok(2.02) is False
+
+    def test_pops_oldest_when_sensitivity_value_breached_and_record_exceeds_horizon_length(
+        self, stopper
+    ):
+        for i in [1, 2, 2.01, 2.02]:
+            stopper.ok(i)
         assert stopper.record == [2, 2.01, 2.02]
