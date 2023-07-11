d71bead01c8f91386a4d71763f7bc00d1ed75f74
Fred Shone <fred.shone@arup.com>
2023-07-11 11:18:39 +0100

    adds utils docstrings

--- pam/utils.py
+++ pam/utils.py
@@ -3,9 +3,9 @@ import os
 from datetime import datetime, timedelta
 from io import BytesIO
 from pathlib import Path
+from typing import Generator, Union
 
 import numpy as np
-from lxml import etree
 from lxml import etree as et
 from s2sphere import CellId
 from shapely.geometry import LineString, Point
@@ -16,7 +16,20 @@ from pam.variables import START_OF_DAY
 DEFAULT_GZIP_COMPRESSION = 6
 
 
-def parse_time(time):
+def parse_time(time: Union[int, str]) -> datetime:
+    """Generic parse time into datetime.
+    Itegers are assumed to be minutes. Strings are assumed to be datetime formatted as (%Y-%m-%d %H:%M:%S).
+
+    Args:
+        time (Union[int, str]): time
+
+    Raises:
+        UserWarning: raised if wrong types provided
+
+    Returns:
+        datetime: datetime
+    """
+
     if isinstance(time, int) or isinstance(time, np.int64):
         return minutes_to_datetime(time)
     if isinstance(time, str):
@@ -26,82 +39,136 @@ def parse_time(time):
     )
 
 
-def minutes_to_datetime(minutes: int):
-    """Convert minutes to datetime
-    :param minutes: int
-    :return: datetime.
+def minutes_to_datetime(minutes: int) -> datetime:
+    """Convert minutes to datetime.
+
+    Args:
+        minutes (int): minutes
+
+    Returns:
+        datetime: datetime
     """
     days, remainder = divmod(minutes, 24 * 60)
     hours, minutes = divmod(remainder, 60)
+    # TODO refactor to reuse timedelta
     return datetime(1900, 1, 1 + days, hours, minutes)
 
 
-def minutes_to_timedelta(minutes: int):
-    """Convert minutes to timedelta
-    :param minutes: int
-    :return: timedelta.
-    """
-    return timedelta(minutes=minutes)
-
-
-def datetime_string_to_datetime(string: str):
+def datetime_string_to_datetime(string: str) -> datetime:
     """Convert datetime formatted string to datetime
-    :param string: str "%Y-%m-%d %H:%M:%S"
-    :return: datetime.
+
+    Args:
+        string (str): time string formatted "%Y-%m-%d %H:%M:%S"
+
+    Returns:
+        datetime: datetime
     """
     return datetime.strptime(string, "%Y-%m-%d %H:%M:%S")
 
 
-def datetime_to_matsim_time(dt):
-    """Convert datetime to matsim format time (08:27:33)
+def minutes_to_timedelta(minutes: int) -> timedelta:
+    """Convert minutes to timedelta.
+
+    Args:
+        minutes (int): minutes
+
+    Returns:
+        timedelta: timedelta
+    """
+    return timedelta(minutes=minutes)
+
+
+def datetime_to_matsim_time(dt: datetime) -> str:
+    """Convert datetime to matsim format time (hh:mm:ss)
     Datetimes beyond 1 day will be converted to hours, eg 25:00:00, for 1am the next day.
-    return dt.strftime("%H:%M:%S").
+
+    Args:
+        dt (datetime): datetime
+
+    Returns:
+        str: MATSim time format (hh:mm:ss)
     """
     return timedelta_to_matsim_time(dt - START_OF_DAY)
 
 
-def matsim_time_to_datetime(string: str) -> datetime:
-    """Convert matsim format time (08:27:33) to datetime.
-    Can read MATSim times for any day of a simulation (ie 25:00:00 is read as 01:00:00 of the next day).
+def timedelta_to_matsim_time(td: timedelta) -> str:
+    """Convert datetime timedelta object to matsim string format (hh:mm:ss).
 
-    :param string: Time from start of the simulation (%H:%M:%S)
+    Args:
+        td (timedelta): timedelta
+
+    Returns:
+        str: MATSim time string (hh:mm:ss)
     """
-    return safe_strptime(string)
-
-
-def timedelta_to_matsim_time(td):
-    """Convert datetime timedelta object to matsim string format (00:00:00)."""
     hours, remainder = divmod(td.total_seconds(), 3600)
     minutes, seconds = divmod(remainder, 60)
     return f"{int(hours):02}:{int(minutes):02}:{int(seconds):02}"
 
 
-def dt_to_s(dt):
-    """Convert datetime to seconds since start of day."""
+def matsim_time_to_datetime(string: str) -> datetime:
+    """Convert matsim format time (hh:mm:ss) to datetime.
+    Can read MATSim times for any day of a simulation (ie 25:00:00 is read as 01:00:00 of the next day).
+
+    Args:
+        string (str): MATSim time from start of the simulation (%H:%M:%S)
+
+    Returns:
+        datetime: datetime
+    """
+    return safe_strptime(string)
+
+
+def dt_to_s(dt: datetime) -> int:
+    """Convert datetime to seconds since start of day.
+
+    Args:
+        dt (datetime): datetime
+
+    Returns:
+        int: seconds as integer
+    """
     return (((dt.hour * 60) + dt.minute) * 60) + dt.second
 
 
-def td_to_s(td):
-    """Convert timedelta to seconds since start of day."""
+def td_to_s(td: timedelta) -> int:
+    """Convert timedelta to seconds since start of day.
+
+    Args:
+        td (timedelta): timedelta
+
+    Returns:
+        int: seconds as integer
+    """
     return (td.days * 86400) + td.seconds
 
 
-def safe_strptime(mt):
+def safe_strptime(mt: str) -> datetime:
     """Safely parse string into datetime, can cope with time strings in format hh:mm:ss
-    if hh > 23 then adds a day.
+    if hh > 24 then adds a day.
+
+    Args:
+        mt (str): MATSim time string (hh:mm:ss or hh::mm)
+
+    Returns:
+        datetime: datetime
     """
-    units = mt.split(":")
-    if len(units) == 3:
-        h, m, s = mt.split(":")
-        return START_OF_DAY + timedelta(hours=int(h), minutes=int(m), seconds=int(s))
-    if len(units) == 2:
-        h, m = mt.split(":")
-        return START_OF_DAY + timedelta(hours=int(h), minutes=int(m))
-    raise UserWarning(f"Unrecognised time format: {mt}")
+    return START_OF_DAY + safe_strpdelta(mt)
 
 
-def safe_strpdelta(mt):
-    """Parse string into timedelta, can cope with time strings in format hh:mm:ss or hh:mm"""
+def safe_strpdelta(mt: str) -> timedelta:
+    """Parse string into timedelta, can cope with time strings in format hh:mm:ss or hh:mm
+    :param mt:
+    :return: timedelta
+
+    Args:
+        mt (str): MATSim time string (hh:mm:ss or hh::mm)
+
+    Raises:
+        UserWarning: Incorrect formatted input
+
+    Returns:
+        timedelta: timedelta
+    """
     units = mt.split(":")
     if len(units) == 3:
         h, m, s = mt.split(":")
@@ -112,20 +179,50 @@ def safe_strpdelta(mt):
     raise UserWarning(f"Unrecognised timedelta format: {mt}")
 
 
-def timedelta_to_hours(td):
+def timedelta_to_hours(td: timedelta) -> float:
+    """Timedelta to hours.
+
+    Args:
+        td (timedelta): timedelta
+
+    Returns:
+        float: hours as float
+    """
     return td.total_seconds() / 3600
 
 
-def matsim_duration_to_hours(mt):
+def matsim_duration_to_hours(mt: str) -> int:
+    """Turn MATSim time string (hh:mm:ss or hh::mm) into hours.
+
+    Args:
+        mt (str): MATSim time string (hh:mm:ss or hh::mm)
+
+    Raises:
+        UserWarning: Incorrect formatted input
+
+    Returns:
+        int: hours as integer
+    """
     mt = mt.split(":")
-    return int(mt.pop()) / 3600 + int(mt.pop()) / 60 + int(mt.pop())
+    if len(mt) == 3:
+        return int(mt.pop()) / 3600 + int(mt.pop()) / 60 + int(mt.pop())
+    if len(mt) == 2:
+        return int(mt.pop()) / 60 + int(mt.pop())
+    raise UserWarning(f"Unrecognised timedelta format: {mt}")
 
 
-def get_linestring(from_point, to_point):
-    """Makes a shapely.geometry.LineString out of two points
-    :param from_point: shapely.geometry.Point or s2sphere.CellId
-    :param to_point: shapely.geometry.Point or s2sphere.CellId
-    :return:
+def get_linestring(from_point: Union[Point, CellId], to_point: Union[Point, CellId]) -> LineString:
+    """ "Makes a shapely.geometry.LineString out of two points.
+
+    Args:
+        from_point (Point): shapely.geometry.Point or s2sphere.CellId
+        to_point (Point): shapely.geometry.Point or s2sphere.CellId
+
+    Raises:
+        TypeError: Failure to parse inputs
+
+    Returns:
+        LineString: LineString
     """
     if all(isinstance(p, CellId) for p in [from_point, to_point]):
         from_point = from_point.to_lat_lng()
@@ -141,11 +238,18 @@ def get_linestring(from_point, to_point):
     return LineString([from_point, to_point])
 
 
-def get_elems(path, tag):
+def get_elems(path: Union[str, Path], tag: str) -> Generator:
     """Wrapper for unzipping and dealing with xml namespaces
-    :param path: xml path string
-    :param tag: The tag type to extract , e.g. 'link'
-    :return: Generator of elements.
+
+    Args:
+        path (Union[str, Path]): xml path
+        tag (str): The tag type to extract , e.g. 'link'
+
+    Returns:
+        Generator:  Generator of elements
+
+    Yields:
+        Generator:  Generator of elements
     """
     target = try_unzip(path)
     tag = get_tag(target, tag)
@@ -153,13 +257,17 @@ def get_elems(path, tag):
     return parse_elems(target, tag)
 
 
-def parse_elems(target, tag):
+def parse_elems(target: Union[BytesIO, str, Path], tag: str) -> Generator:
     """Traverse the given XML tree, retrieving the elements of the specified tag.
-    :param target: Target xml, either BytesIO object or string path
-    :param tag: The tag type to extract , e.g. 'link'
-    :return: Generator of elements.
+
+    Args:
+        target (Union[BytesIO, str, Path]): Target xml, either BytesIO object or string path
+        tag (str): The tag type to extract , e.g. 'link'
+
+    Yields:
+        Generator: Generator of elements.
     """
-    doc = etree.iterparse(target, tag=tag)
+    doc = et.iterparse(target, tag=tag)
     for _, element in doc:
         yield element
         element.clear()
@@ -168,10 +276,14 @@ def parse_elems(target, tag):
     del doc
 
 
-def try_unzip(path):
+def try_unzip(path: Union[str, Path]) -> Union[BytesIO, str, Path]:
     """Attempts to unzip xml at given path, if fails, returns path
-    :param path: xml path string
-    :return: either BytesIO object or string path.
+
+    Args:
+        path (Union[str, Path]): xml path
+
+    Returns:
+        Union[BytesIO, str, Path]: BytesIO object or path if already unzipped
     """
     try:
         with gzip.open(path) as unzipped:
@@ -182,14 +294,21 @@ def try_unzip(path):
         return path
 
 
-def get_tag(target, tag):
+def get_tag(target: Union[BytesIO, str, Path], tag: str) -> str:
     """Check for namespace declaration. If they exists return tag string
     with namespace [''] ie {namespaces['']}tag. If no namespaces declared
-    return original tag
+    return original tag.
     TODO Not working with iterparse, generated elem also have ns which is dealt with later.
+
+    Args:
+        target (Union[BytesIO, str, Path]): Target xml, either BytesIO object or path
+        tag (str): The tag type to extract , e.g. 'link'
+
+    Returns:
+        str: tag
     """
     nsmap = {}
-    doc = etree.iterparse(target, events=("end", "start-ns"))
+    doc = et.iterparse(target, events=("end", "start-ns"))
     count = 0
     for event, element in doc:
         count += 1
@@ -205,13 +324,15 @@ def get_tag(target, tag):
         return tag
 
 
-def strip_namespace(elem):
+def strip_namespace(elem: et.Element):
     """Strips namespaces from given xml element
-    :param elem: xml element
-    :return: xml element.
+
+    Args:
+        elem (et.Element): xml element
     """
     if elem.tag.startswith("{"):
-        elem.tag = elem.tag.split("}", 1)[1]  # strip namespace
+        # strip namespace
+        elem.tag = elem.tag.split("}", 1)[1]
     for k in elem.attrib.keys():
         if k.startswith("{"):
             k2 = k.split("}", 1)[1]
@@ -221,8 +342,15 @@ def strip_namespace(elem):
         strip_namespace(child)
 
 
-def create_crs_attribute(coordinate_reference_system):
-    """Create a CRS attribute as expected by MATSim's ProjectionUtils.getCRS."""
+def create_crs_attribute(coordinate_reference_system: str) -> et.Element:
+    """Create a CRS attribute as expected by MATSim's ProjectionUtils.getCRS.
+
+    Args:
+        coordinate_reference_system (str): coordinate reference system
+
+    Returns:
+        et.Element: CRS attribute as xml element
+    """
     attributes_element = et.Element("attributes")
     crs_attribute = et.SubElement(
         attributes_element,
@@ -233,20 +361,50 @@ def create_crs_attribute(coordinate_reference_system):
     return attributes_element
 
 
-def is_xml(location):
+def is_xml(location: Union[str, Path]) -> bool:
+    """Checks if file is xml based on extension.
+
+    Args:
+        location (Union[str, Path]): file path
+
+    Returns:
+        bool: is xml?
+    """
     return Path(location).suffix.lower() == ".xml"
 
 
-def is_gzip(location):
+def is_gzip(location: Union[str, Path]) -> bool:
+    """Checks if file is gzipped based on extension.
+
+    Args:
+        location (Union[str, Path]): file path
+
+    Returns:
+        bool: is gzipped?
+    """
     suffix = Path(location).suffix.lower()
     return suffix == ".gz" or suffix == ".gzip"
 
 
-def create_local_dir(directory):
+def create_local_dir(directory: Union[str, Path]):
+    """Safely create new directory.
+    TODO this can be replaced with pathlib I think
+
+    Args:
+        directory (Union[str, Path]): new directory path
+    """
     if not os.path.exists(directory):
         os.makedirs(directory)
 
 
-def xml_tree(content):
-    tree = etree.tostring(content, pretty_print=True, xml_declaration=False, encoding="UTF-8")
+def xml_tree(content: et.Element) -> str:
+    """Retrun pretty formatted string of xml tree.
+
+    Args:
+        content (et.Element): xml
+
+    Returns:
+        str: pretty string of xml
+    """
+    tree = et.tostring(content, pretty_print=True, xml_declaration=False, encoding="UTF-8")
     return tree
