856faf3c941985cbf21250eb06886af39c10796e
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2023-09-04 16:53:43 +0100

    Try a different data filepath in memory profiling

--- tests/test_100_memory_profiling.py
+++ tests/test_100_memory_profiling.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pandas as pd
 import pytest
 
@@ -6,12 +8,14 @@ from pam import read
 BENCHMARK_MEM = "2890.59 MB"
 BENCHMARK_SECONDS = 285
 
+data_dir = Path(__file__).parent / "test_data"
+
 
 @pytest.mark.limit_memory(BENCHMARK_MEM)
 @pytest.mark.timeout(BENCHMARK_SECONDS)
 @pytest.mark.high_mem
 def test_activity_loader():
-    trips = pd.read_csv(pytest.test_data_dir / "extended_travel_diaries.csv.gz")
-    attributes = pd.read_csv(pytest.test_data_dir / "extended_persons_data.csv.gz")
+    trips = pd.read_csv(data_dir / "extended_travel_diaries.csv.gz")
+    attributes = pd.read_csv(data_dir / "extended_persons_data.csv.gz")
     attributes.set_index("pid", inplace=True)
     read.load_travel_diary(trips, attributes)
