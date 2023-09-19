80daf7a6e2932ab81a87f5f0b7a1fd64ec4e4f9c
Theodore-Chatziioannou <Theodore.Chatziioannou@arup.com>
2023-09-19 09:33:39 +0000

    refine matrix ipf

--- pam/planner/ipf.py
+++ pam/planner/ipf.py
@@ -1,22 +1,47 @@
+import warnings
 from typing import Optional
 
 import numpy as np
 
 
-def safe_divide(m, s):
-    return np.divide(m, s, out=np.zeros(m.shape, dtype=np.float64), where=s != 0)
+def safe_divide(x: np.array, y: np.array) -> np.array:
+    """Safely divide two arrays. When dividing by zero, the result is set to zero.
 
 
-def get_scaling_factor(X: np.array, sel_dim: int, marginals: np.array):
+    Args:
+        x (np.array): Nominator array.
+        y (np.array): Denominator array.
+
+    Returns:
+        np.array: Divided array.
+    """
+    return np.divide(x, y, out=np.zeros(x.shape, dtype=np.float64), where=y != 0)
+
+
+def get_scaling_factor(X: np.ndarray, sel_dim: int, marginals: np.array) -> np.ndarray:
+    """Get the scaling factors required to match a set of marginals for a selected matrix dimension.
+        The returned matrix will have the same dimensions as the input matrix
+
+    Args:
+        X (np.ndarray): The matrix to be scaled.
+        sel_dim (int): The matrix dimension that corresponds to the marginals.
+        marginals (np.array): The target values for the selected dimension.
+
+    Returns:
+        np.ndarray: A matrix that can be multiplied by the input matrix
+            in order to get a new matrix that matches the maginals for the selected dimension.
+    """
     other_dims = tuple(i for i in range(X.ndim) if i != sel_dim)
     totals = X.sum(axis=other_dims)
+    if ((totals == 0) & (totals != marginals)).sum():
+        warnings.warn("Zero-cell issue found! Please check the seed matrix totals.", UserWarning)
     scaling_factor = safe_divide(marginals, totals)
     scaling_factor = np.expand_dims(scaling_factor, other_dims)
 
     return scaling_factor
 
 
-def get_max_error(X: np.array, marginals: list[np.array]) -> float:
+def get_max_error(X: np.ndarray, marginals: list[np.array]) -> float:
     """Get the maximum absolute percentage between a matrix and a set of marginals.
 
     Args:
@@ -27,9 +52,10 @@ def get_max_error(X: np.array, marginals: list[np.array]) -> float:
         float: the maximum error value
     """
     max_error = 0
-    for sel_dim in range(X.ndim):
-        scaling_factor = get_scaling_factor(X, sel_dim, marginals[sel_dim])
-        max_error = max(max_error, abs(scaling_factor.max() - 1))
+    for dim in range(X.ndim):
+        scaling_factor = get_scaling_factor(X, dim, marginals[dim])
+        max_error = max(max_error, abs(scaling_factor - 1).max())
+
     return max_error
 
 
--- tests/test_28_planner_ipf.py
+++ tests/test_28_planner_ipf.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from pam.planner.ipf import ipf
+from pam.planner import ipf
 
 
 @pytest.fixture
@@ -12,9 +12,26 @@ def marginals():
 
 def test_random_seed_matches_marginals(marginals):
     X = np.zeros(tuple(map(len, marginals))) + 0.001
-    fitted = ipf(X, marginals, max_iterations=10)
+    fitted = ipf.ipf(X, marginals, max_iterations=10)
     np.testing.assert_almost_equal(fitted.sum((1, 2)), marginals[0])
+    np.testing.assert_almost_equal(fitted.sum((0, 2)), marginals[1])
+    np.testing.assert_almost_equal(fitted.sum((0, 1)), marginals[2])
 
 
-def test_ipf_maxes_patience_out():
-    pass
+def test_ipf_maxes_patience_out(mocker):
+    mocker.patch.object(ipf, "get_scaling_factor", return_value=np.ones((2, 2)) * 2)
+    ipf.ipf(np.zeros((2, 2)) + 0.1, np.ones((2, 2)), max_iterations=0)
+    # only called once for each dimension at the start
+    assert ipf.get_scaling_factor.call_count == 2
+
+
+def test_ipf_matrix_retains_shape(marginals):
+    X = np.zeros(tuple(map(len, marginals))) + 0.001
+    fitted = ipf.ipf(X, marginals, max_iterations=10)
+    assert fitted.shape == X.shape
+
+
+def test_ipf_raises_zero_cell_problem(marginals):
+    X = np.zeros(tuple(map(len, marginals)))
+    with pytest.warns(UserWarning):
+        ipf.ipf(X, marginals, max_iterations=10)
