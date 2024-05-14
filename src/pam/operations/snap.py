1cc8c3cbc6de88f17ea8ae72ffc68a34819adfad
Thedore-Chatziioannou <Theodore.Chatziioannou@arup.com>
2024-05-14 10:50:57 +0000

    facility link snapping

---
+++ src/pam/operations/snap.py
@@ -0,0 +1,48 @@
+""" Methods for snapping elements to the network or facilities. """
+
+import geopandas as gp
+
+from pam.core import Population
+from pam.read import read_matsim
+from pam.write import write_matsim
+
+
+def snap_facilities_to_network(
+    population: Population,
+    network: gp.GeoDataFrame,
+    link_id_field: str = "id"
+) -> None:
+    """Snaps activity facilities to a network geometry (in-place).
+
+    Args:
+        population (Population): A PAM popualtion.
+        network (gp.GeoDataFrame): A network geometry shapefile.
+        link_id_field (str, optional): The link ID field to use in the network shapefile. Defaults to "id".
+    """
+    link_ids = network[link_id_field]
+    for _, _, person in population.people():
+        for act in person.activities:
+            link_id = link_ids[network.distance(act.location.loc).argmin()]
+            act.location.link = link_id
+
+def run_facility_link_snapping(
+        path_population_in: str,
+        path_population_out: str,
+        path_network_geometry: str,
+        link_id_field: str = "id"
+)->None:
+    """Reads a population, snaps activity facilities to a network geometry, and saves the results.
+
+    Args:
+        path_population_in (str): Path to a PAM population.
+        path_population_out (str): The path to save the output population.
+        path_network_geometry (str): Path to the network geometry file.
+        link_id_field (str, optional): The link ID field to use in the network shapefile. Defaults to "id".
+    """
+    population = read_matsim(path_population_in)
+    if path_network_geometry.endswith(".parquet"):
+        network = gp.read_parquet(path_network_geometry)
+    else:
+        network = gp.read_file(path_network_geometry)
+    snap_facilities_to_network(population=population, network=network, link_id_field=link_id_field)
+    write_matsim(population=population, plans_path=path_population_out)
---
+++ tests/test_29_snap.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+
+import geopandas as gp
+from pam.operations.snap import run_facility_link_snapping, snap_facilities_to_network
+from pam.read import read_matsim
+
+TEST_DATA_DIR = Path(__file__).parent / "test_data"
+
+
+def test_add_snapping_adds_link_attribute(population_heh):
+    network=gp.read_file(os.path.join(TEST_DATA_DIR, "test_link_geometry.geojson"))
+    for _, _, person in population_heh.people():
+        for act in person.activities:
+            assert act.location.link is None
+
+    snap_facilities_to_network(population=population_heh, network=network)
+    for _, _, person in population_heh.people():
+        for act in person.activities:
+            assert act.location.link is not None
+
+def test_links_resnapped(tmpdir):
+    path_out=os.path.join(tmpdir, "pop_snapped.xml")
+    run_facility_link_snapping(
+        path_population_in=os.path.join(TEST_DATA_DIR, "1.plans.xml"), 
+        path_population_out=path_out, 
+        path_network_geometry=os.path.join(TEST_DATA_DIR, "test_link_geometry.geojson")
+    )
+    assert os.path.exists(path_out)
+    pop_snapped = read_matsim(path_out)
+    for _, _, person in pop_snapped.people():
+        for act in person.activities:
+            assert "link-" in act.location.link
