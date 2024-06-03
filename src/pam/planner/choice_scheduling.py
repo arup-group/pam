e188e750d74d9593251f33d99067c9daa0793ed5
Theodore-Chatziioannou <Theodore.Chatziioannou@arup.com>
2024-06-03 16:53:53 +0000

    gaussian mixture example

--- src/pam/planner/choice_scheduling.py
+++ src/pam/planner/choice_scheduling.py
@@ -2,6 +2,12 @@ from typing import Optional
 
 import numpy as np
 from tensorflow import keras
+import tensorflow_probability as tfp
+import tf_keras as tfk
+
+tfd = tfp.distributions
+tfpl = tfp.layers
+tfkl = tfk.layers
 
 from pam.core import Population
 from pam.planner.encoder import PlansSequenceEncoder
@@ -64,5 +70,102 @@ class ScheduleModelSimple:
         y_pred = np.zeros(shape=encoder.durations.shape)
         for i in range(1, y_pred.shape[1]):
             y_pred[:, i] = self.model.predict([X, y_pred[:, :i]])[:, -1, 0]
+
+        return y_pred
+
+
+class ActivityDurationRegression:
+    def __init__(self, acts: np.array, durations: np.array) -> None:
+        """Model to predict durations of a set of activities.
+
+        Args:
+            acts (np.array): Activity tokens. Shape: (n, 1).
+            durations (np.array): Durations. Shape: (n, 1).
+        """
+        self.acts = acts
+        self.durations = durations
+
+        # set up model
+        inputs = keras.layers.Input(shape=(1,))
+        h1 = keras.layers.Dense(50, activation="relu")(inputs)
+        h2 = keras.layers.Dense(20, activation="relu")(h1)
+        outputs = keras.layers.Dense(1, activation="relu")(h2)
+        model = keras.models.Model(inputs=inputs, outputs=outputs)
+        model.compile(optimizer="adam", loss="mean_squared_error", metrics=["accuracy"])
+        model.summary()
+
+        self.model = model
+
+    def fit(self, epochs: int = 20):
+        """Fit the Neural Network model.
+
+        Args:
+            epochs (int, optional): Number of epochs to run. Defaults to 20.
+        """
+        self.history = self.model.fit(self.acts, self.durations, epochs=epochs)
+
+    def predict(self, acts: np.array) -> np.array:
+        """Predict durations
+
+        Args:
+            acts (np.array): Act tokens. Shape: (n, 1).
+
+        Returns:
+            np.array: Durations. Shape: (n, 1)
+        """
+        y_pred = self.model.predict(acts)
+
+        return y_pred
+
+
+class ActivityDurationMixture:
+    def __init__(
+        self, acts: np.array, durations: np.array, n_components: Optional[int] = 2
+    ) -> None:
+        """Mixture Density Model for predicting durations of a set of activities as a multimodal distribution.
+
+        Args:
+            acts (np.array): Activity tokens. Shape: (n, 1).
+            durations (np.array): Durations. Shape: (n, 1).
+            n_components (Optional[int], optional): Number of components of the Gaussian Mixture. Defaults to 2.
+        """
+        self.acts = acts
+        self.durations = durations
+        self.n_components = n_components
+
+        event_shape = [1]
+        params_size = tfp.layers.MixtureNormal.params_size(n_components, event_shape)
+        inputs = tfkl.Input(shape=(1,))
+        h1 = tfkl.Dense(50, activation="relu")(inputs)
+        h2 = tfkl.Dense(20, activation="relu")(h1)
+        h3 = tfkl.Dense(params_size, activation=None)(h2)
+        outputs = tfpl.MixtureNormal(n_components, event_shape, name="output")(h3)
+
+        model = tfk.Model(inputs=inputs, outputs=outputs)
+        model.compile(
+            optimizer="adam", loss=lambda y, model: -model.log_prob(y), metrics=["accuracy"]
+        )
+        model.summary()
+
+        self.model = model
+
+    def fit(self, epochs: Optional[int] = 20):
+        """Fit the Mixture Density Network model.
+
+        Args:
+            epochs (Optional[int], optional): Number of epochs to run. Defaults to 20.
+        """
+        self.history = self.model.fit(self.acts, self.durations, epochs=epochs)
+
+    def predict(self, acts: np.array) -> np.array:
+        """Predict durations
+
+        Args:
+            acts (np.array): Act tokens. Shape: (n, 1).
+
+        Returns:
+            np.array: Durations. Shape: (n, 1)
+        """
+        y_pred = self.model.predict(acts)
 
         return y_pred
--- tests/test_29_planner_scheduling.py
+++ tests/test_29_planner_scheduling.py
@@ -1,12 +1,22 @@
 import numpy as np
 import pytest
-from pam.planner.choice_scheduling import ScheduleModelSimple
+from pam.planner.choice_scheduling import (
+    ActivityDurationMixture,
+    ActivityDurationRegression,
+    ScheduleModelSimple,
+)
+from pam.planner.encoder import PlansSequenceEncoder
 from tensorflow import keras
 
 
 @pytest.fixture
 def model_simple(population_simple) -> ScheduleModelSimple:
     return ScheduleModelSimple(population_simple)
+
+
+@pytest.fixture
+def plans_encoded(population_simple) -> PlansSequenceEncoder:
+    return PlansSequenceEncoder(population_simple)
 
 
 def test_start_end_tokens(model_simple):
@@ -22,3 +32,25 @@ def test_prediction_shape_matches_input(model_simple, population_simple):
 
 def test_model_built(model_simple):
     assert isinstance(model_simple.model, keras.models.Model)
+
+
+def test_activity_duration_regression(plans_encoded):
+    acts = plans_encoded.acts[:, [0]]
+    durations = plans_encoded.durations[:, 0]
+    model = ActivityDurationRegression(acts=acts, durations=durations)
+    model.fit(epochs=1)
+    y_pred = model.predict(acts)
+    assert (y_pred < 0).sum() == 0
+    assert (y_pred > 1).sum() == 0
+    assert y_pred.shape == (len(durations), 1)
+
+
+def test_activity_duration_mixture(plans_encoded):
+    acts = plans_encoded.acts[:, [0]]
+    durations = plans_encoded.durations[:, 0]
+    model = ActivityDurationMixture(acts=acts, durations=durations)
+    model.fit(epochs=1)
+    y_pred = model.predict(acts)
+    assert (y_pred < 0).sum() == 0
+    assert (y_pred > 1).sum() == 0
+    assert y_pred.shape == (len(durations), 1)
